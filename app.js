16b4490f923187604170d076fc50b6e0eb6d8ad5
Zilong Guan <zguan317@gmail.com>
2021-05-10 20:32:41 -0400

    Cards looping are correctly

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  // const recipes = await Recipe.find({});
+  const recipes = await Recipe.find({});
   // res.render('home', { recipes });
-  res.render('home2');
+  res.render('home2', { recipes });
 });
 
 //?Search request
